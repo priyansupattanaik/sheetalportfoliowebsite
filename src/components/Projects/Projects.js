2f4d90d9e2db3f6df39b9c5be1de2e97c38bf635
sheetal2024 <sheetalsharma2517@gmail.com>
2024-07-30 22:07:42 +0530

    1

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import snakegame from "../../Assets/Projects/snakegame.jpeg";
+import nyra from "../../Assets/Projects/nyra.jpeg";
+import quiz from "../../Assets/Projects/quiz.jpeg";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          MY <strong className="purple">WORKS </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={snakegame}
+              title="Snake Game"
+              description=""
+              demoLink="https://snakegamexyz.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={nyra}
+              isBlog={false}
+              title="AI Chat Application"
+              description=""
+              demoLink="https://nyra.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={quiz}
+              isBlog={false}
+              title="Quiz Application"
+              description=""
+              demoLink="https://mindsprint.netlify.app//"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
